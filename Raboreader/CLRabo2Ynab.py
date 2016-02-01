8cefa613679a08b9f42c76436ba739d190150c92
Tbutje <timo.koole@gmail.com>
2016-02-01 21:10:58 +0100

    Make reader class

---
+++ Raboreader/CLRabo2Ynab.py
@@ -0,0 +1,30 @@
+'''
+Created on 1 feb. 2016
+
+@author: Timo
+'''
+
+class CLRabo2Ynab(object):
+    '''
+    classdocs
+    '''
+    file_in = 'init'
+    dir_out = 'init'
+
+    def __init__(self, params, file_in, dir_out):
+        '''
+        Constructor
+        '''
+    def read(self):
+        print(5)
+    
+    def convert(self):
+        print(5)
+        
+    def write(self):
+        print(1)    
+        
+ 
+        
+      
+        
--- Raboreader/main.py
+++ Raboreader/main.py
@@ -4,5 +4,44 @@ Created on 1 feb. 2016
 @author: Timo
 '''
 
+import csv, sys
+from Raboreader.CLRabo2Ynab import CLRabo2Ynab
+
 if __name__ == '__main__':
-    pass
+    filename_in = 'C:/Users/Timo/Documents/LiClipse Workspace/Raboreader/resource/transactions.csv'
+    date        = []; 
+    payee       = [];
+    category    = [];
+    memo        = [];
+    outflow     = [];
+    inflow      = [];
+
+    Converter = CLRabo2Ynab( )
+    
+    Converter.read( filename_in )
+    Converter.convert( )
+#     Converter.write( )
+    
+    
+    try:
+        with open(filename_in) as file_in:
+                
+            reader = csv.reader(file_in)
+    #             Skip first line
+            next(reader, None)
+            for row in reader:
+                date.append(row[2])
+    except IOError:
+        sys.exit(
+                    "input file error",
+                    "input file not found\nplease check the input file name"
+                )
+        
+    print('succes')
+            
+    
+                
+            
+                    
+            
+        
